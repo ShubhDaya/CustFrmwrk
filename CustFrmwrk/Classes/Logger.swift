b5e236863faeb817356f3528d25a79716754680b
ShubhDaya <shubhamdaya9@gmail.com>
2023-03-31 14:56:58 +0530

    initial commit

---
+++ CustFrmwrk/Classes/Logger.swift
@@ -0,0 +1,20 @@
+//
+//  logger.swift
+//  CustFrmwrk
+//
+//  Created by ADMIN on 31/03/23.
+//
+
+import Foundation
+class Logger{
+    public init(){}
+
+    public func printLog(){
+        print("hello world")
+    }
+
+    func privateMethod(){
+        print("private")
+    }
+
+}
---
+++ Example/Pods/Target Support Files/CustFrmwrk/CustFrmwrk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CustFrmwrk : NSObject
+@end
+@implementation PodsDummy_CustFrmwrk
+@end
